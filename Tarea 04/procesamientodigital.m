b1f1c7798b8f815552008f3ecbca3d84fad71a38
AlejandraMLM <2816733570108@ingenieria.usac.edu.gt>
2025-08-12 20:44:05 -0600

    procesamientodigital.m

--- Tarea 04/procesamientodigital.m
+++ Tarea 04/procesamientodigital.m
@@ -7,6 +7,7 @@ end
 #Conexión a la base de datos
 conn = pq_connect(setdbopts('dbname','postgres','host','localhost','port','5432','user','postgres','password','koala'));
 
+#Variable para el menu principal
 opcion = 0;
 
 while opcion ~= 5
@@ -21,6 +22,7 @@ while opcion ~= 5
 
   switch opcion
       case 1
+        #Grabar audio
           try
               duracion = input('Ingrese la duracion de la grabacion en segundos: ');
               disp('Comenzando la grabacion...');
@@ -46,6 +48,7 @@ while opcion ~= 5
           end
 
       case 2
+        #Reproducir audio
           try
               disp('Reproduciendo audio...');
               [data, fs] = audioread('audio.wav');
@@ -55,6 +58,7 @@ while opcion ~= 5
           end
 
       case 3
+      #Graficar señal
           try
               disp('Graficando señal...');
               [data, fs] = audioread('audio.wav');
@@ -68,14 +72,15 @@ while opcion ~= 5
           end
 
       case 4
+      #Grafica espectro de frecuencia
           try
               disp('Graficando espectro de frecuencia...');
-              [audio, fs] = audioread('audio.wav');
-              N = length(audio);
-              f = linspace(0, fs/2, N/2+1);
-              ventana = hann(N);
-              Sxx = pwelch(audio, ventana, 0, N, fs);
-              plot(f, 10*log10(Sxx(1:N/2+1)));
+              [audio, fs] = audioread('audio.wav');  #Lee la señal desde el archivo .wav
+              N = length(audio);  #Numero de muestras de la señal
+              f = linspace(0, fs/2, N/2+1);  #Vector de frecuencia
+              ventana = hann(N);  #Ventana de Hann para reducir el efecto de las discontinuidades al calcular la FFT
+              Sxx = pwelch(audio, ventana, 0, N, fs);  #Densidad espectral de potencia
+              plot(f, 10*log10(Sxx(1:N/2+1)));  #Grafica el espectro de frecuencia en dB
               xlabel('Frecuencia (Hz)');
               ylabel('Densidad espectral de potencia (dB/Hz)');
               title('Espectro de Frecuencia de la señal grabada');
@@ -84,8 +89,10 @@ while opcion ~= 5
           end
 
       case 5
+      #Salir
           disp('Saliendo del programa...');
       otherwise
           disp('Opción no válida.');
   end
 end
+
